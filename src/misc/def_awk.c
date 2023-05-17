db0ddf507fc82d7c55d6f12a00d25f834424e02c
AlexPeshkoff <alexander.peshkoff@gmail.com>
2023-05-17 19:10:30 +0300

    POSIX-only fix for #7582: Missing isc_info_end in Firebird.pas. Appropriate fix for windows build is needed.
    (cherry picked from commit 6707c7c2ccd54b3577aa485409ee88a2760e2a92)

--- src/misc/def_awk.c
+++ src/misc/def_awk.c
@@ -18,7 +18,7 @@ int main()
 		toks[2] = NULL;
 		for (n = 0; n < 3; ++n)
 		{
-			toks[n] = strtok(p, " \t\n\r");
+			toks[n] = strtok(p, " \t\n\r,");
 			p = NULL;
 			if (!toks[n])
 				break;
@@ -29,7 +29,12 @@ int main()
 			continue;
 
 		/* skip unknown statements */
-		if (strcmp(toks[0], "#define") != 0)
+		char* name = NULL;
+		if (strcmp(toks[0], "#define") == 0)	//	#define isc_info_end            1
+			name = toks[1];
+		else if (strcmp(toks[1], "=") == 0)		//	isc_info_db_id          = 4
+			name = toks[0];
+		if (! name)
 			continue;
 
 		/* skip unknown constants */
@@ -43,11 +48,11 @@ int main()
 
 		/* output correct constant */
 		if (*toks[2] == '-')
-			printf("\t%s = %s;\n", toks[1], toks[2]);
+			printf("\t%s = %s;\n", name, toks[2]);
 		else if (strncmp(toks[2], "0x", 2) == 0)
-			printf("\t%s = $%s;\n", toks[1], toks[2] + 2);
+			printf("\t%s = $%s;\n", name, toks[2] + 2);
 		else
-			printf("\t%s = byte(%s);\n", toks[1], toks[2]);
+			printf("\t%s = byte(%s);\n", name, toks[2]);
 	}
 
 	return 0;
