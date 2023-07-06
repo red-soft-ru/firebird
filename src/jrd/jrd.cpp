b27c25a02351010824be035ebcce140d2c583cec
AlexPeshkoff <alexander.peshkoff@gmail.com>
2023-07-06 13:22:10 +0300

    Fixed #7626: Segfault when new attachment is done to shutting down database
    (cherry picked from commit 6749faa584720c8e9cf6a1b4b7b1e81e69f2db28)

--- src/jrd/jrd.cpp
+++ src/jrd/jrd.cpp
@@ -1435,6 +1435,7 @@ JAttachment* JProvider::internalAttach(CheckStatusWrapper* user_status, const ch
 		PathName org_filename, expanded_name;
 		bool is_alias = false;
 		MutexEnsureUnlock guardDbInit(dbInitMutex, FB_FUNCTION);
+		LateRefGuard lateBlocking(FB_FUNCTION);
 
 		try
 		{
@@ -1555,6 +1556,7 @@ JAttachment* JProvider::internalAttach(CheckStatusWrapper* user_status, const ch
 			}
 
 			EngineContextHolder tdbb(user_status, jAtt, FB_FUNCTION, AttachmentHolder::ATT_DONT_LOCK);
+			lateBlocking.lock(jAtt->getStable()->getBlockingMutex(), jAtt->getStable());
 
 			attachment->att_crypt_callback = getDefCryptCallback(cryptCallback);
 			attachment->att_client_charset = attachment->att_charset = options.dpb_interp;
@@ -2462,6 +2464,7 @@ JAttachment* JProvider::createDatabase(CheckStatusWrapper* user_status, const ch
 		PathName org_filename, expanded_name;
 		bool is_alias = false;
 		Firebird::RefPtr<const Config> config;
+		LateRefGuard lateBlocking(FB_FUNCTION);
 
 		try
 		{
@@ -2574,6 +2577,7 @@ JAttachment* JProvider::createDatabase(CheckStatusWrapper* user_status, const ch
 			dbbGuard.lock(SYNC_EXCLUSIVE);
 
 			EngineContextHolder tdbb(user_status, jAtt, FB_FUNCTION, AttachmentHolder::ATT_DONT_LOCK);
+			lateBlocking.lock(jAtt->getStable()->getBlockingMutex(), jAtt->getStable());
 
 			attachment->att_crypt_callback = getDefCryptCallback(cryptCallback);
 
