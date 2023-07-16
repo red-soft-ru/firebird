4071211e55b909efbc2bbc725c351401bf48db79
Vlad Khorsun <hvlad@users.sourceforge.net>
2023-07-16 18:32:38 +0300

    Better handling errors when databases built. Fallback to "copy" when "mklink" failed.

--- builds/win32/make_boot.bat
+++ builds/win32/make_boot.bat
@@ -271,16 +271,27 @@ goto :EOF
 @mkdir %FB_GEN_DIR%\dbs 2>nul
 
 @echo Create security5.fdb...
-@echo create database '%FB_GEN_DB_DIR%\dbs\security5.fdb'; | "%FB_BIN_DIR%\isql" -q
+@echo create database '%FB_GEN_DB_DIR%\dbs\security5.fdb'; | "%FB_BIN_DIR%\isql" -q > nul
+if errorlevel 1 call :boot2 databases & goto :EOF
+
 @echo Apply security.sql...
-@"%FB_BIN_DIR%\isql" -q %FB_GEN_DB_DIR%/dbs/security5.fdb -i %FB_ROOT_PATH%\src\dbs\security.sql
-@mklink %FB_GEN_DIR%\dbs\security.fdb %FB_GEN_DIR%\dbs\security5.fdb
-rem @copy %FB_GEN_DIR%\dbs\security5.fdb %FB_GEN_DIR%\dbs\security.fdb > nul
+@"%FB_BIN_DIR%\isql" -q %FB_GEN_DB_DIR%/dbs/security5.fdb -i %FB_ROOT_PATH%\src\dbs\security.sql > nul
+if errorlevel 1 call :boot2 databases & goto :EOF
+
+@mklink %FB_GEN_DIR%\dbs\security.fdb %FB_GEN_DIR%\dbs\security5.fdb > nul
+if errorlevel 1 (
+  @copy %FB_GEN_DIR%\dbs\security5.fdb %FB_GEN_DIR%\dbs\security.fdb > nul
+)
+if errorlevel 1 call :boot2 databases & goto :EOF
 
 @echo Creating metadata.fdb...
-@echo create database '%FB_GEN_DB_DIR%/dbs/metadata.fdb'; | "%FB_BIN_DIR%\isql" -q -sqldialect 1
-@mklink %FB_GEN_DIR%\dbs\yachts.lnk %FB_GEN_DIR%\dbs\metadata.fdb
-rem @copy %FB_GEN_DIR%\dbs\metadata.fdb %FB_GEN_DIR%\dbs\yachts.lnk > nul
+@echo create database '%FB_GEN_DB_DIR%/dbs/metadata.fdb'; | "%FB_BIN_DIR%\isql" -q -sqldialect 1 > nul
+if errorlevel 1 call :boot2 databases & goto :EOF
+
+@mklink %FB_GEN_DIR%\dbs\yachts.lnk %FB_GEN_DIR%\dbs\metadata.fdb > nul
+if errorlevel 1 (
+  @copy %FB_GEN_DIR%\dbs\metadata.fdb %FB_GEN_DIR%\dbs\yachts.lnk > nul
+)
 if errorlevel 1 call :boot2 databases
 
 @goto :EOF
