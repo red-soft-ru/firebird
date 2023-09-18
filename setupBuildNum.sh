a54a3079d9ef8d562be3870eeb58cbe1cdf299d7
Dmitry Yemanov <dyemanov@users.noreply.github.com>
2023-09-18 11:37:13 +0300

    Reset buildno for v6

--- setupBuildNum.sh
+++ setupBuildNum.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+v60Filter="4f47587a72ac8bece190b529cd4c50417f772226"		# Sep 16 2023, increment build number
 v50Filter="054431fce7650c0d58df3f9a4100ea98d215486e"		# May 5 2021, Implemented Batch::cancel() over the wire
 v40Filter="bb46a0e8310de196104bcca78fda2e0c1ea77bb5"		# Feb 28 2016, Set up the new version
 v30Offset=15471
@@ -13,7 +14,7 @@ Filter="$3"
 
 if [ "$Filter" ]
 then
-	Range="$Filter..$Branch"
+	Range="$Filter..$Branch" # this notation excludes commit $Filter, so it should not belong to our range
 else
 	Range="$Branch"
 fi
@@ -57,7 +58,8 @@ rm -f $errFile
 
 git fetch --all
 
-processBranch master 0 $v50Filter
+processBranch master 0 $v60Filter
+processBranch v5.0-release 0 $v50Filter
 processBranch v4.0-release 0 $v40Filter
 processBranch B3_0_Release $v30Offset
 processBranch B2_5_Release $v25Offset
