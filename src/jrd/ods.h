7c822e41f9628151627bfec6a4a53fdd7523c38b
Dmitry Yemanov <dyemanov@users.noreply.github.com>
2023-09-24 19:02:20 +0300

    Raise minor ODS number for v6

--- src/jrd/ods.h
+++ src/jrd/ods.h
@@ -123,8 +123,9 @@ const USHORT ODS_CURRENT12		= 0;
 // Minor versions for ODS 13
 
 const USHORT ODS_CURRENT13_0	= 0;	// Firebird 4.0 features
-const USHORT ODS_CURRENT13_1	= 1;	// Firebird 4.1 features
-const USHORT ODS_CURRENT13		= 1;
+const USHORT ODS_CURRENT13_1	= 1;	// Firebird 5.0 features
+const USHORT ODS_CURRENT13_2	= 2;	// Firebird 6.0 features
+const USHORT ODS_CURRENT13		= 2;
 
 // useful ODS macros. These are currently used to flag the version of the
 // system triggers and system indices in ini.e
@@ -146,7 +147,7 @@ const USHORT ODS_11_2		= ENCODE_ODS(ODS_VERSION11, 2);
 const USHORT ODS_12_0		= ENCODE_ODS(ODS_VERSION12, 0);
 const USHORT ODS_13_0		= ENCODE_ODS(ODS_VERSION13, 0);
 const USHORT ODS_13_1		= ENCODE_ODS(ODS_VERSION13, 1);
-const USHORT ODS_13_2		= ODS_13_1;	//// FIXME: Review - ODS for v6.
+const USHORT ODS_13_2		= ENCODE_ODS(ODS_VERSION13, 2);
 
 const USHORT ODS_FIREBIRD_FLAG = 0x8000;
 
@@ -174,7 +175,7 @@ const USHORT ODS_RELEASED = ODS_CURRENT13_0;	// The lowest stable minor version
 const USHORT ODS_CURRENT = ODS_CURRENT13;		// The highest defined minor version
 												// number for this ODS_VERSION!
 
-const USHORT ODS_CURRENT_VERSION = ODS_13_1;	// Current ODS version in use which includes
+const USHORT ODS_CURRENT_VERSION = ODS_13_2;	// Current ODS version in use which includes
 												// both major and minor ODS versions!
 
 
