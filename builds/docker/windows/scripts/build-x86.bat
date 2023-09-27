9d6bd05f25b63d19d26c614332bcaf4efa7ee4f8
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2023-09-27 07:50:45 -0300

    Fix Windows installer automation.

--- builds/docker/windows/scripts/build-x86.bat
+++ builds/docker/windows/scripts/build-x86.bat
@@ -12,3 +12,4 @@ call run_all.bat PDB
 call run_tests.bat
 
 copy C:\firebird-build\builds\install_images\* C:\firebird\builds\install_images
+xcopy /h /e /i /q C:\firebird-build\output_Win32_release C:\firebird\output_Win32_release
