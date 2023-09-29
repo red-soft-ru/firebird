3afd116d5ff8015e21a326a5c68bfed529149468
GitHub Action <action@github.com>
2023-09-29 20:13:52 +0000

    increment build number

--- src/jrd/build_no.h
+++ src/jrd/build_no.h
@@ -3,16 +3,16 @@
                *** DO NOT EDIT ***
   TO CHANGE ANY INFORMATION IN HERE PLEASE
   EDIT src/misc/writeBuildNum.sh
-  FORMAL BUILD NUMBER:1234
+  FORMAL BUILD NUMBER:1235
 */
 
-#define PRODUCT_VER_STRING "5.0.0.1234"
-#define FILE_VER_STRING "WI-V5.0.0.1234"
-#define LICENSE_VER_STRING "WI-V5.0.0.1234"
-#define FILE_VER_NUMBER 5, 0, 0, 1234
+#define PRODUCT_VER_STRING "5.0.0.1235"
+#define FILE_VER_STRING "WI-V5.0.0.1235"
+#define LICENSE_VER_STRING "WI-V5.0.0.1235"
+#define FILE_VER_NUMBER 5, 0, 0, 1235
 #define FB_MAJOR_VER "5"
 #define FB_MINOR_VER "0"
 #define FB_REV_NO "0"
-#define FB_BUILD_NO "1234"
+#define FB_BUILD_NO "1235"
 #define FB_BUILD_TYPE "V"
 #define FB_BUILD_SUFFIX "Firebird 5.0 Release Candidate 1"
