cdd2eae93eaca8c4c9a093ba2335242ef890396e
GitHub Action <action@github.com>
2023-11-02 20:14:13 +0000

    increment build number

--- src/jrd/build_no.h
+++ src/jrd/build_no.h
@@ -3,16 +3,16 @@
                *** DO NOT EDIT ***
   TO CHANGE ANY INFORMATION IN HERE PLEASE
   EDIT src/misc/writeBuildNum.sh
-  FORMAL BUILD NUMBER:1259
+  FORMAL BUILD NUMBER:1261
 */
 
-#define PRODUCT_VER_STRING "5.0.0.1259"
-#define FILE_VER_STRING "WI-V5.0.0.1259"
-#define LICENSE_VER_STRING "WI-V5.0.0.1259"
-#define FILE_VER_NUMBER 5, 0, 0, 1259
+#define PRODUCT_VER_STRING "5.0.0.1261"
+#define FILE_VER_STRING "WI-V5.0.0.1261"
+#define LICENSE_VER_STRING "WI-V5.0.0.1261"
+#define FILE_VER_NUMBER 5, 0, 0, 1261
 #define FB_MAJOR_VER "5"
 #define FB_MINOR_VER "0"
 #define FB_REV_NO "0"
-#define FB_BUILD_NO "1259"
+#define FB_BUILD_NO "1261"
 #define FB_BUILD_TYPE "V"
-#define FB_BUILD_SUFFIX "Firebird 5.0 Release Candidate 1"
+#define FB_BUILD_SUFFIX "Firebird 5.0 RC 2"
--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -9,7 +9,7 @@ BuildType=V
 MajorVer=5
 MinorVer=0
 RevNo=0
-BuildNum=1259
+BuildNum=1261
 
 NowAt=`pwd`
 cd `dirname $0`
