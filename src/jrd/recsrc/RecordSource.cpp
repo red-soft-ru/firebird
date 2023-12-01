e4900f972e02d5d869ca497c2b7eb0159ea7c6ec
Vlad Khorsun <hvlad@users.sourceforge.net>
2023-12-01 12:12:37 +0200

    Fixed error due to refactoring in commit 37b23f193b. Shown by failed test for core-5222.

--- src/jrd/recsrc/RecordSource.cpp
+++ src/jrd/recsrc/RecordSource.cpp
@@ -306,7 +306,7 @@ bool RecordStream::refetchRecord(thread_db* tdbb) const
 
 	if (rpb->rpb_runtime_flags & RPB_refetch)
 	{
-		if (VIO_refetch_record(tdbb, rpb, transaction, false, false))
+		if (VIO_refetch_record(tdbb, rpb, transaction, true, false))
 		{
 			rpb->rpb_runtime_flags &= ~RPB_refetch;
 			return true;
