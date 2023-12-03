ab6c56f9a2a0dead4c80bfd3c31a2e22dffd764e
Dmitry Yemanov <dyemanov@users.noreply.github.com>
2023-12-03 18:49:44 +0300

    Fixed #7903: Unexpected Results when Using CASE-WHEN with LEFT JOIN

--- src/dsql/ExprNodes.h
+++ src/dsql/ExprNodes.h
@@ -577,6 +577,11 @@ public:
 	virtual void genBlr(DsqlCompilerScratch* dsqlScratch);
 	virtual void make(DsqlCompilerScratch* dsqlScratch, dsc* desc);
 
+	virtual bool ignoreNulls(const StreamList& /*streams*/) const
+	{
+		return false;
+	}
+
 	virtual void getDesc(thread_db* tdbb, CompilerScratch* csb, dsc* desc);
 	virtual ValueExprNode* copy(thread_db* tdbb, NodeCopier& copier) const;
 	virtual ValueExprNode* pass2(thread_db* tdbb, CompilerScratch* csb);
