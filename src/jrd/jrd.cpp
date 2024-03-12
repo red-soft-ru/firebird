884c7fb7aad8e75099062970ae9694a81730d4b9
Vlad Khorsun <hvlad@users.sourceforge.net>
2024-03-12 21:09:44 +0200

    Let Applier to shutdown correctly on bug-checked database

--- src/jrd/jrd.cpp
+++ src/jrd/jrd.cpp
@@ -6494,8 +6494,7 @@ void JReplicator::freeEngineData(Firebird::CheckStatusWrapper* user_status)
 {
 	try
 	{
-		EngineContextHolder tdbb(user_status, this, FB_FUNCTION);
-		check_database(tdbb);
+		EngineContextHolder tdbb(user_status, this, FB_FUNCTION, AttachmentHolder::ATT_NO_SHUTDOWN_CHECK);
 
 		try
 		{
