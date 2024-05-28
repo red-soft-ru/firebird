f48409b167d377e86e3b230739531dd47a747ca4
Vlad Khorsun <hvlad@users.sourceforge.net>
2024-05-28 14:28:17 +0300

    This should fix bug #8138 : Bugcheck when replicator state is changed concurrently

--- src/jrd/Attachment.cpp
+++ src/jrd/Attachment.cpp
@@ -1115,6 +1115,18 @@ void Attachment::checkReplSetLock(thread_db* tdbb)
 
 void Attachment::invalidateReplSet(thread_db* tdbb, bool broadcast)
 {
+	if (broadcast)
+	{
+		// Signal other attachments about the changed state
+		if (att_repl_lock->lck_logical == LCK_none)
+			LCK_lock(tdbb, att_repl_lock, LCK_EX, LCK_WAIT);
+		else
+			LCK_convert(tdbb, att_repl_lock, LCK_EX, LCK_WAIT);
+	}
+
+	if (att_flags & ATT_repl_reset)
+		return;
+
 	att_flags |= ATT_repl_reset;
 
 	if (att_relations)
@@ -1124,15 +1136,6 @@ void Attachment::invalidateReplSet(thread_db* tdbb, bool broadcast)
 			if (relation)
 				relation->rel_repl_state.invalidate();
 		}
-	}
-
-	if (broadcast)
-	{
-		// Signal other attachments about the changed state
-		if (att_repl_lock->lck_logical == LCK_none)
-			LCK_lock(tdbb, att_repl_lock, LCK_EX, LCK_WAIT);
-		else
-			LCK_convert(tdbb, att_repl_lock, LCK_EX, LCK_WAIT);
 	}
 
 	LCK_release(tdbb, att_repl_lock);
