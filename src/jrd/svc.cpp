f713060b150c53a4ccfb258b7e9cfe4428bb13f2
Vlad Khorsun <hvlad@users.sourceforge.net>
2024-06-08 11:26:46 +0300

    Undone part of recent fix for #8151 as it prevents trace session from tracing own start as shown by failed QA tests: tests\bugs\core_3168_test.py tests\bugs\core_3658_test.py tests\functional\services\test_role_in_service_attachment.py

--- src/jrd/svc.cpp
+++ src/jrd/svc.cpp
@@ -1958,8 +1958,6 @@ void Service::start(USHORT spb_length, const UCHAR* spb_data)
 		Arg::Gds(isc_bad_svc_handle).raise();
 	}
 
-	bool needTraceEvent = false;
-
 	try
 	{
 	if (!svcShutdown)
@@ -2098,8 +2096,6 @@ void Service::start(USHORT spb_length, const UCHAR* spb_data)
 		svc_status->init();
 	}
 
-	needTraceEvent = svc_trace_manager->needs(ITraceFactory::TRACE_EVENT_SERVICE_START);
-
 	if (serv->serv_thd)
 	{
 		svc_flags &= ~(SVC_evnt_fired | SVC_finished);
@@ -2138,7 +2134,7 @@ void Service::start(USHORT spb_length, const UCHAR* spb_data)
 	}	// try
 	catch (const Firebird::Exception& ex)
 	{
-		if (needTraceEvent)
+		if (svc_trace_manager->needs(ITraceFactory::TRACE_EVENT_SERVICE_START))
 		{
 			FbLocalStatus status_vector;
 			ex.stuffException(&status_vector);
@@ -2154,7 +2150,7 @@ void Service::start(USHORT spb_length, const UCHAR* spb_data)
 		throw;
 	}
 
-	if (needTraceEvent)
+	if (svc_trace_manager->needs(ITraceFactory::TRACE_EVENT_SERVICE_START))
 	{
 		TraceServiceImpl service(this);
 		this->svc_trace_manager->event_service_start(&service,
