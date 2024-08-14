red-soft-ru/firebird

commit 0f8e7c185cba10dd4ebe94f6ef306ac19a43a794

Small optimization - avoid use of std::function<> where not necessary

src/common/classes/auto.h
@@ -32,7 +32,6 @@
 #define CLASSES_AUTO_PTR_H
 
 #include <stdio.h>
-#include <functional>
 
 namespace Firebird {
 
@@ -318,11 +317,11 @@ private:
 	T oldValue;
 };
 
-
+template <typename F>
 class Cleanup
 {
 public:
-	Cleanup(std::function<void()> clFunc)
+	Cleanup(F&& clFunc)
 		: clean(clFunc)
 	{ }
 
@@ -332,7 +331,7 @@ public:
 	}
 
 private:
-	std::function<void()> clean;
+	F& clean;
 };
 
 } //namespace Firebird

src/yvalve/YObjects.h
@@ -36,6 +36,8 @@
 #include "../common/MsgMetadata.h"
 #include "../common/classes/ClumpletWriter.h"
 
+#include <functional>
+
 namespace Why
 {
 

commit 60a6f2a733881498d52d0bf1b839b9a81faa54a3

Fixed UB caused by 0f8e7c18 - avoid storing dangling pointer to lambda

src/common/classes/auto.h
@@ -331,7 +331,25 @@ public:
 	}
 
 private:
-	F& clean;
+	F clean;
+};
+
+class CleanupFunction
+{
+	typedef void Func();
+
+public:
+	CleanupFunction(Func* clFunc)
+		: clean(clFunc)
+	{ }
+
+	~CleanupFunction()
+	{
+		clean();
+	}
+
+private:
+	Func* clean;
 };
 
 } //namespace Firebird

src/common/classes/init.cpp
@@ -145,7 +145,7 @@ namespace
 #ifndef DEBUG_INIT
 
 	// This instance ensures dtors run when program exits
-	Firebird::Cleanup global(allClean);
+	Firebird::CleanupFunction global(allClean);
 
 #endif //DEBUG_INIT
 