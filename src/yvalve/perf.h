74c3725e0eed2b95ed43c7671e91b34366d746fa
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2024-09-29 14:48:05 -0300

    perf.h: a public file that depends on private file should not be a public file

--- src/yvalve/perf.h
+++ src/yvalve/perf.h
@@ -28,8 +28,8 @@
  *
  */
 
-#ifndef FIREBIRD_PERF_H
-#define FIREBIRD_PERF_H
+#ifndef JRD_PERF_H
+#define JRD_PERF_H
 
 #include <stdio.h>
 
@@ -116,5 +116,5 @@ typedef struct perf64
 
 #include "../yvalve/perf_proto.h"
 
-#endif /* FIREBIRD_PERF_H */
+#endif /* JRD_PERF_H */
 
