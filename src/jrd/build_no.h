e95c128e0e826cab5a06d9eb20406cb7adff58e1
GitHub Action <action@github.com>
2024-10-04 20:16:39 +0000

    increment build number

--- src/jrd/build_no.h
+++ src/jrd/build_no.h
@@ -3,16 +3,16 @@
                *** DO NOT EDIT ***
   TO CHANGE ANY INFORMATION IN HERE PLEASE
   EDIT src/misc/writeBuildNum.sh
-  FORMAL BUILD NUMBER:483
+  FORMAL BUILD NUMBER:485
 */
 
-#define PRODUCT_VER_STRING "6.0.0.483"
-#define FILE_VER_STRING "WI-T6.0.0.483"
-#define LICENSE_VER_STRING "WI-T6.0.0.483"
-#define FILE_VER_NUMBER 6, 0, 0, 483
+#define PRODUCT_VER_STRING "6.0.0.485"
+#define FILE_VER_STRING "WI-T6.0.0.485"
+#define LICENSE_VER_STRING "WI-T6.0.0.485"
+#define FILE_VER_NUMBER 6, 0, 0, 485
 #define FB_MAJOR_VER "6"
 #define FB_MINOR_VER "0"
 #define FB_REV_NO "0"
-#define FB_BUILD_NO "483"
+#define FB_BUILD_NO "485"
 #define FB_BUILD_TYPE "T"
 #define FB_BUILD_SUFFIX "Firebird 6.0 Initial"
--- src/misc/writeBuildNum.sh
+++ src/misc/writeBuildNum.sh
@@ -9,7 +9,7 @@ BuildType=T
 MajorVer=6
 MinorVer=0
 RevNo=0
-BuildNum=483
+BuildNum=485
 
 NowAt=`pwd`
 cd `dirname $0`
