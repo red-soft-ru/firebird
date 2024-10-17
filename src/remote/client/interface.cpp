46cc4c75d68509a42a62e1b7270eb0a9d2543a4f
Vlad Khorsun <hvlad@users.sourceforge.net>
2024-10-17 10:54:16 +0300

    Post fix for #8256. port should be NULLified after forced disconnect.

--- src/remote/client/interface.cpp
+++ src/remote/client/interface.cpp
@@ -7399,9 +7399,6 @@ static rem_port* analyze(ClntAuthBlock& cBlock, PathName& attach_name, unsigned 
  *
  **************************************/
 
-	rem_port* port = NULL;
-	int inet_af = AF_UNSPEC;
-
 	cBlock.loadClnt(pb, &parSet);
 	pb.deleteWithTag(parSet.auth_block);
 
@@ -7413,8 +7410,12 @@ static rem_port* analyze(ClntAuthBlock& cBlock, PathName& attach_name, unsigned 
 	Auth::setLegacySSP(legacySSP);
 #endif
 
+	rem_port* port;
 	while (true)
 	{
+		port = NULL;
+		int inet_af = AF_UNSPEC;
+
 		authenticateStep0(cBlock);
 		const NoCaseString savePluginName(cBlock.plugins.name());
 
