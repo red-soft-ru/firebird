46d8f5f4a9d307fbaa36d80a7f5a4c19a2ebce36
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2024-10-16 23:22:31 -0300

    Update boost to version 1.86.0.

--- extern/boost/boost/math/special_functions/detail/fp_traits.hpp
+++ extern/boost/boost/math/special_functions/detail/fp_traits.hpp
@@ -34,16 +34,6 @@ With these techniques, the code could be simplified.
 #define BOOST_MATH_ENDIAN_BIG_BYTE BOOST_ENDIAN_BIG_BYTE
 #define BOOST_MATH_ENDIAN_LITTLE_BYTE BOOST_ENDIAN_LITTLE_BYTE
 
-#elif (__cplusplus >= 202002L || _MSVC_LANG >= 202002L)
-
-#if __has_include(<bit>)
-#include <bit>
-#define BOOST_MATH_ENDIAN_BIG_BYTE (std::endian::native == std::endian::big)
-#define BOOST_MATH_ENDIAN_LITTLE_BYTE (std::endian::native == std::endian::little)
-#else
-#error Missing <bit> header. Please disable standalone mode, and file an issue at https://github.com/boostorg/math
-#endif
-
 #elif defined(_WIN32)
 
 #define BOOST_MATH_ENDIAN_BIG_BYTE 0
@@ -493,7 +483,7 @@ private:
 // size_to_precision is a type switch for converting a C++ floating point type
 // to the corresponding precision type.
 
-template<int n, bool fp> struct size_to_precision
+template<size_t n, bool fp> struct size_to_precision
 {
    typedef unknown_precision type;
 };
