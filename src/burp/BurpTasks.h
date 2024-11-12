01c64fe1029cea731d4f706f6599643d00748388
Vlad Khorsun <hvlad@users.sourceforge.net>
2024-11-12 13:57:02 +0200

    Fixed bug #8315 : Crash at database restore due to failed system call

--- src/burp/BurpTasks.h
+++ src/burp/BurpTasks.h
@@ -545,7 +545,13 @@ public:
 			if (!m_mutex.tryEnter(FB_FUNCTION))
 				return;
 
+			fb_assert(m_locked >= 0);
+			const bool lockedByMe = (m_locked != 0);
+
 			m_mutex.leave();
+
+			if (!lockedByMe)
+				return;
 		}
 
 		fb_assert(m_locked > 0);
