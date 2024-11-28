05e39419dfdfbd9f85065f23d7516a6c35bb57c2
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2024-11-27 23:08:48 -0300

    Improvement to the Windows test runner.

--- builds/win32/run_tests.bat
+++ builds/win32/run_tests.bat
@@ -5,8 +5,8 @@
 @call setenvvar.bat %*
 @if errorlevel 1 (goto :END)
 
-@%FB_BIN_DIR%\common_test --log_level=all
-@%FB_BIN_DIR%\engine_test --log_level=all
-@%FB_BIN_DIR%\isql_test --log_level=all
+@%FB_BIN_DIR%\common_test --log_level=all || exit /b
+@%FB_BIN_DIR%\engine_test --log_level=all || exit /b
+@%FB_BIN_DIR%\isql_test --log_level=all || exit /b
 
 :END
